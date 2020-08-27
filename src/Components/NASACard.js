058f737ca88101b466f86bb4428e117e443812ca
Robert Stevens <robertstevens2015@gmail.com>
2020-08-27 18:27:22 -0500

    DAY 2! Project completed!

--- src/Components/NASACard.js
+++ src/Components/NASACard.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+    border: 1px solid darkgrey;
+    background-color: lightgrey;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const TitleContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const Title = styled.h2`
+    font-size: 2.5rem;
+`;
+
+const DescContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const Desc = styled.p`
+    font-size: 1rem;
+    width: 70%;
+    border: 2px solid darkgrey;
+    background-color: lightgrey;
+    text-align: justify;
+`;
 
 const NasaInfo = props => {
     console.log(props);
 
     return (
         <div className='nasaCard' key={props.data}>
-            <h2>{props.title}</h2>
-            <h3>{props.date}</h3>
-            <img src={props.image} alt='We are NASA!'/>
-            <p>{props.info}</p>
+            <MainContainer>
+                <TitleContainer>
+                    <Title className='title'>{props.title}</Title>
+                </TitleContainer>
+                <div className="img-cont">
+                    <img src={props.image} alt="space NASA" />
+                </div>
+                <DescContainer>
+                    <Desc>{props.info}</Desc>
+                </DescContainer>
+            </MainContainer>
         </div>
     )
 }
